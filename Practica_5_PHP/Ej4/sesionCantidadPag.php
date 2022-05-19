79bd4845f16841766a00971c6990fedb084b2d3d
Augusto Amadeo López <67442428+augustolopez12@users.noreply.github.com>
2022-05-18 23:38:46 -0300

    Create sesionCantidadPag.php

---
+++ Practica_5_PHP/Ej4/sesionCantidadPag.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+?>
+<html>
+<body>
+<a href="sesionContador.php"></a>
+<?php
+echo "Has visitado " . ($_SESSION["contador"]) . " páginas";
+?>
+<br>
+<br>
+<a href="sesionContador.php">Ir a contador</a>
+</body>
+</html>
+© 2022 GitHub, Inc.
