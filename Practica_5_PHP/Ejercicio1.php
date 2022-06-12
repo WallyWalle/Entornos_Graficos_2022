831ee4b25f40f24c15ab47befce82b62cdded956
Augusto Amadeo López <67442428+augustolopez12@users.noreply.github.com>
2022-06-12 14:59:53 -0300

    Update Ejercicio1.php

--- Practica_5_PHP/Ejercicio1.php
+++ Practica_5_PHP/Ejercicio1.php
@@ -19,7 +19,7 @@
     $headers = "MIME-Version: 1.0\r\n";
     $headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
     //dirección del remitente concatenada con otras direcciones 
-    $headers .= "From: fedenavos@gmail.com\r\n"; 
+    $headers .= "From: augustolope99@gmail.com\r\n"; 
     
     mail($destinatario,$asunto,$cuerpo,$headers)
 ?>
