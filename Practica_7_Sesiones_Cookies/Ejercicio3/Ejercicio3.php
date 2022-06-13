34cba4a208fe46daf544227adf69036c9674fe35
Augusto Amadeo López <67442428+augustolopez12@users.noreply.github.com>
2022-06-13 18:40:43 -0300

    Update Ejercicio3.php

--- Practica_7_Sesiones_Cookies/Ejercicio3/Ejercicio3.php
+++ Practica_7_Sesiones_Cookies/Ejercicio3/Ejercicio3.php
@@ -9,7 +9,6 @@
 <body>
     
     <h1>Ejercicio 3 - Practica 7</h1>
-    <h2>Federico Navós</h2>
     Crear un formulario que solicite la carga del nombre de usuario. Cuando se presione un botón
     crear una cookie para dicho usuario. Luego cada vez que ingrese al formulario mostrar el último
     nombre de usuario ingresado. 
